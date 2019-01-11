88a6b4efbb64168a6ffa548f5d96f5c887e98e11
Marcela Bomfim <mbomfim@live.com>
2019-01-11 13:09:41 -0200

    Install and configured redux

--- packages/react-scripts/template/src/App.jsx
+++ packages/react-scripts/template/src/App.jsx
@@ -1,11 +1,15 @@
 import React, { Fragment } from 'react'
+import { Provider } from 'react-redux';
 
 import Home from './containers/Home';
 import GlobalStyle from './utils/styles/global';
+import store from './store';
 
 export default () => (
-  <Fragment>
-    <Home />
-    <GlobalStyle />
-  </Fragment>
+  <Provider store={store}>
+    <Fragment>
+      <Home />
+      <GlobalStyle />
+    </Fragment>
+  </Provider>
 );
